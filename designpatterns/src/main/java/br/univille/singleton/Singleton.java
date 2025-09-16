b625c3c6898c567bdb780ef21034c44e9400b93e
Christyan <127155755+Christyassolini@users.noreply.github.com>
2025-09-16 00:49:14 +0000

    WIP: publisher

--- designpatterns/src/main/java/br/univille/singleton/Singleton.java
+++ designpatterns/src/main/java/br/univille/singleton/Singleton.java
@@ -1,16 +1,29 @@
 package br.univille.singleton;
 
 public class Singleton {
-    // variavel global, static ele faz com que ele seja alocado na memória antes que o objeto
+    // variavel global
     private static Singleton instance;
+    private String segredo;
+
+    public String getSegredo() {
+        return segredo;
+    }
+
+    public void setSegredo(String segredo) {
+        this.segredo = segredo;
+    }
 
     private Singleton(){
-
+        System.out.println("Construtor Singleton");
     }
+
     public static Singleton getInstance(){
+        System.out.println("getInstance");
         if(instance == null){
+            System.out.println("new Singleton");
             instance = new Singleton();
         }
+
         return instance;
     }
 }
